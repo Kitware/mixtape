8eabec8e62def25ec99149f3cbc4f1cb90d65422
Brianna Major <brianna.major@kitware.com>
2025-02-05 11:26:45 -0500

    Replace argparse with djclick

--- mixtape/core/management/commands/inference.py
+++ mixtape/core/management/commands/inference.py
@@ -1,9 +1,8 @@
-import argparse
 from pathlib import Path
 
+import djclick as click
 from gymnasium.wrappers import AtariPreprocessing, FrameStack
 import numpy as np
-from django.core.management.base import BaseCommand
 from ray.rllib.algorithms.algorithm import Algorithm
 import yaml
 
@@ -12,104 +11,95 @@ from mixtape.ray_utils.constants import ExampleEnvs
 from mixtape.ray_utils.utils import is_gymnasium_env, register_environment
 
 
-class Command(BaseCommand):
-    help = 'Runs inference on the specified trained environment.'
+@click.command()
+@click.argument('checkpoint_path', type=click.Path(exists=True))
+@click.option(
+    '-e',
+    '--env_name',
+    type=click.Choice(
+        [
+            'knights_archers_zombies_v10',
+            'pistonball_v6',
+            'cooperative_pong_v5',
+            'BattleZone-v5',
+            'Berzerk-v5',
+            'ChopperCommand-v5',
+        ]
+    ),
+    default=ExampleEnvs.PZ_KnightsArchersZombies.value,
+    help='The PettingZoo or Gymnasium environment to use.',
+)
+@click.option(
+    '-f',
+    '--config_file',
+    type=click.File('r'),
+    default=None,
+    help='Arguments to configure the environment.',
+)
+@click.option(
+    '-p',
+    '--parallel',
+    is_flag=True,
+    help='All agents have simultaneous actions and observations.',
+)
+def inference(checkpoint_path, env_name, config_file, parallel):
+    """Run inference on the specified trained environment."""
+    config_dict = {}
+    if config_file is not None:
+        config_dict = yaml.safe_load(config_file)
+    env_config = config_dict.get('env_config', {})
+    env = register_environment(env_name, env_config, parallel)
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            'checkpoint_path', type=str, help='The path to a checkpoint directory to restore from.'
-        )
-        parser.add_argument(
-            '-e',
-            '--env_name',
-            type=ExampleEnvs,
-            choices=[
-                'knights_archers_zombies_v10',
-                'pistonball_v6',
-                'cooperative_pong_v5',
-                'BattleZone-v5',
-                'Berzerk-v5',
-                'ChopperCommand-v5',
-            ],
-            default=ExampleEnvs.PZ_KnightsArchersZombies,
-            help='The PettingZoo or Gymnasium environment to use.',
-        )
-        parser.add_argument(
-            '-f',
-            '--config_file',
-            type=argparse.FileType('r'),
-            help='Arguments to configure the environment.',
-        )
-        parser.add_argument(
-            '-p',
-            '--parallel',
-            action='store_true',
-            help='All agents have simultaneous actions and observations. Defaults to True.',
-        )
-        parser.add_argument('--aec', dest='parallel', action='store_false')
-        parser.set_defaults(parallel=False)
+    callback = InferenceLoggingCallbacks(env)
 
-    def handle(self, *args, **options):
-        config_dict = {}
-        if options.get('config_file') is not None:
-            config_dict = yaml.safe_load(options['config_file'])
-        env_name = options['env_name'].value
-        env_config = config_dict.get('env_config', {})
-        env = register_environment(env_name, env_config, options['parallel'])
+    # Restore a trained model checkpoint for inference
+    checkpoint_path = str(Path(checkpoint_path).resolve())
+    algorithm = Algorithm.from_checkpoint(checkpoint_path)
 
-        callback = InferenceLoggingCallbacks(env)
+    callback.on_begin_inference()
+    if is_gymnasium_env(env_name):
+        env = AtariPreprocessing(env, grayscale_obs=True, scale_obs=False, frame_skip=1)
+        env = FrameStack(env, num_stack=4)
+        done = False
+        observation, _ = env.reset()  # `reset` returns observation for single agent in Gym envs
 
-        # Restore a trained model checkpoint for inference
-        checkpoint_path = str(Path(options['checkpoint_path']).resolve())
-        algorithm = Algorithm.from_checkpoint(checkpoint_path)
+        while not done:
+            observation = np.transpose(observation, (1, 2, 0))
+            action = algorithm.compute_single_action(observation)
+            # Step the environment forward with the computed actions
+            observation, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            callback.on_compute_action(
+                {'agent_0': action}, {'agent_0': reward}, {'agent_0': np.array(observation)}
+            )
+        callback.on_complete_inference(env_name, parallel=False)
+    elif parallel:
+        # `reset` returns observations for all agents in parallel envs
+        observations, _ = env.reset()
+        done = {agent: False for agent in env.agents}
 
-        callback.on_begin_inference()
+        # Loop until all agents are done (terminated or truncated)
+        while any(not finished for finished in done.values()):
+            actions = {
+                agent: algorithm.compute_single_action(obs) for agent, obs in observations.items()
+            }
+            # Step the environment forward with the computed actions
+            observations, rewards, terminations, truncations, infos = env.step(actions)
+            done = {agent: terminations[agent] or truncations[agent] for agent in env.agents}
+            callback.on_compute_action(actions, rewards, observations)
+        callback.on_complete_inference(env_name)
+    else:
+        # AEC environment (agent-iterating environment)
+        env.reset()
 
-        if is_gymnasium_env(env_name):
-            env = AtariPreprocessing(env, grayscale_obs=True, scale_obs=False, frame_skip=1)
-            env = FrameStack(env, num_stack=4)
-            done = False
-            observation, _ = env.reset()  # `reset` returns observation for single agent in Gym envs
-
-            while not done:
-                observation = np.transpose(observation, (1, 2, 0))
+        # Loop over agents in the environment using the agent iterator
+        for agent in env.agent_iter():
+            observation, reward, termination, truncation, info = env.last()
+            if termination or truncation:
+                action = None  # No action needed if the agent is done
+            else:
                 action = algorithm.compute_single_action(observation)
-                # Step the environment forward with the computed actions
-                observation, reward, terminated, truncated, info = env.step(action)
-                done = terminated or truncated
-                callback.on_compute_action(
-                    {'agent_0': action}, {'agent_0': reward}, {'agent_0': np.array(observation)}
-                )
-            callback.on_complete_inference(env_name, parallel=False)
-        elif options['parallel']:
-            observations, _ = (
-                env.reset()
-            )  # `reset` returns observations for all agents in parallel envs
-            done = {agent: False for agent in env.agents}
-
-            # Loop until all agents are done (terminated or truncated)
-            while any(not finished for finished in done.values()):
-                actions = {
-                    agent: algorithm.compute_single_action(obs)
-                    for agent, obs in observations.items()
-                }
-                # Step the environment forward with the computed actions
-                observations, rewards, terminations, truncations, infos = env.step(actions)
-                done = {agent: terminations[agent] or truncations[agent] for agent in env.agents}
-                callback.on_compute_action(actions, rewards, observations)
-            callback.on_complete_inference(env_name)
-        else:
-            # AEC environment (agent-iterating environment)
-            env.reset()
-
-            # Loop over agents in the environment using the agent iterator
-            for agent in env.agent_iter():
-                observation, reward, termination, truncation, info = env.last()
-                if termination or truncation:
-                    action = None  # No action needed if the agent is done
-                else:
-                    action = algorithm.compute_single_action(observation)
-                env.step(action)  # Step the environment forward with the action
-                callback.on_compute_action({agent: action}, {agent: reward}, {agent: observation})
-            callback.on_complete_inference(env_name, parallel=False)
-        env.close()  # Close the environment to free resources
+            env.step(action)  # Step the environment forward with the action
+            callback.on_compute_action({agent: action}, {agent: reward}, {agent: observation})
+        callback.on_complete_inference(env_name, parallel=False)
+    env.close()  # Close the environment to free resources
--- mixtape/core/management/commands/training.py
+++ mixtape/core/management/commands/training.py
@@ -1,7 +1,6 @@
-import argparse
 from pathlib import Path
 
-from django.core.management.base import BaseCommand
+import djclick as click
 from ray.rllib.algorithms.dqn import DQNConfig
 from ray.rllib.algorithms.ppo import PPOConfig
 from ray.tune import run
@@ -9,124 +8,118 @@ import yaml
 
 from mixtape.ray_utils.callbacks import CustomLoggingCallbacks
 from mixtape.ray_utils.constants import ExampleEnvs, SupportedAlgorithm
-from mixtape.ray_utils.utils import register_environment
+from mixtape.ray_utils.utils import is_gymnasium_env, register_environment
 
 
-class Command(BaseCommand):
-    help = 'Runs training on the specified environment.'
+@click.command()
+@click.option(
+    '-e',
+    '--env_name',
+    type=click.Choice(
+        [
+            'knights_archers_zombies_v10',
+            'pistonball_v6',
+            'cooperative_pong_v5',
+            'BattleZone-v5',
+            'Berzerk-v5',
+            'ChopperCommand-v5',
+        ]
+    ),
+    default=ExampleEnvs.PZ_KnightsArchersZombies.value,
+    help='The PettingZoo or Gymnasium environment to use.',
+)
+@click.option(
+    '-a',
+    '--algorithm',
+    type=click.Choice(['PPO', 'DQN']),
+    default=SupportedAlgorithm.PPO.value,
+    help='The RLlib algorithm to use.',
+)
+@click.option(
+    '-p',
+    '--parallel',
+    is_flag=True,
+    help='All agents have simultaneous actions and observations.',
+)
+@click.option('-g', '--num_gpus', type=float, default=0, help='Number of GPUs to use.')
+@click.option(
+    '-t',
+    '--training_iteration',
+    type=int,
+    default=100,
+    help='Number of training iterations to run.',
+)
+@click.option(
+    '-f',
+    '--config_file',
+    type=click.File('r'),
+    default=None,
+    help='Arguments to configure the environment.',
+)
+def training(env_name, algorithm, parallel, num_gpus, training_iteration, config_file):
+    """Run training on the specified environment."""
+    config_dict = {}
+    if config_file is not None:
+        config_dict = yaml.safe_load(config_file)
+    env_config = config_dict.get('env_config', {})
+    parallel = False if is_gymnasium_env(env_name) else parallel
+    register_environment(env_name, env_config, parallel)
 
-    def add_arguments(self, parser):
-        parser.add_argument(
-            '-e',
-            '--env_name',
-            type=ExampleEnvs,
-            choices=[
-                'knights_archers_zombies_v10',
-                'pistonball_v6',
-                'cooperative_pong_v5',
-                'BattleZone-v5',
-                'Berzerk-v5',
-                'ChopperCommand-v5',
-            ],
-            default=ExampleEnvs.PZ_KnightsArchersZombies,
-            help='The PettingZoo or Gymnasium environment to use.',
-        )
-        parser.add_argument(
-            '-a',
-            '--algorithm',
-            type=SupportedAlgorithm,
-            choices=['PPO', 'DQN'],
-            default=SupportedAlgorithm.PPO,
-            help='The RLlib algorithm to use.',
-        )
-        parser.add_argument(
-            '-p',
-            '--parallel',
-            action='store_true',
-            help='All agents have simultaneous actions and observations. Defaults to True.',
-        )
-        parser.add_argument('--aec', dest='parallel', action='store_false')
-        parser.set_defaults(parallel=False)
-        parser.add_argument(
-            '-g', '--num_gpus', type=float, default=0, help='Number of GPUs to use.'
-        )
-        parser.add_argument(
-            '-t',
-            '--training_iteration',
-            type=int,
-            default=100,
-            help='Number of training iterations to run.',
-        )
-        parser.add_argument(
-            '-f',
-            '--config_file',
-            type=argparse.FileType('r'),
-            help='Arguments to configure the environment and/or training.',
-        )
+    # Ensure all arguments default to empty dict, not None
+    training_args = config_dict.get('training_args', {})
+    env_args = config_dict.get('env_args', {})
+    framework_args = config_dict.get('framework_args', {})
 
-    def handle(self, *args, **options):
-        config_dict = {}
-        if options.get('config_file') is not None:
-            config_dict = yaml.safe_load(options['config_file'])
-        env_name = options['env_name'].value
-        env_config = config_dict.get('env_config', {})
-        register_environment(env_name, env_config, options['parallel'])
+    # Set general purpose defaults
+    training_args.setdefault('train_batch_size', 512)
+    training_args.setdefault('lr', 2e-5)
+    training_args.setdefault('gamma', 0.99)
 
-        # Ensure all arguments default to empty dict, not None
-        training_args = config_dict.get('training_args', {})
-        env_args = config_dict.get('env_args', {})
-        framework_args = config_dict.get('framework_args', {})
+    # Set algorithm specific defaults
+    if algorithm == SupportedAlgorithm.PPO:
+        ppo_defaults = {
+            'lambda_': 0.9,
+            'use_gae': True,
+            'clip_param': 0.4,
+            'grad_clip': None,
+            'entropy_coeff': 0.1,
+            'vf_loss_coeff': 0.25,
+            'sgd_minibatch_size': 64,
+            'num_sgd_iter': 10,
+        }
+        training_args = ppo_defaults | training_args
 
-        # Set general purpose defaults
-        training_args.setdefault('train_batch_size', 512)
-        training_args.setdefault('lr', 2e-5)
-        training_args.setdefault('gamma', 0.99)
+    # Setup config with callbacks, debugging, training, etc.
+    alg = PPOConfig() if algorithm == SupportedAlgorithm.PPO else DQNConfig()
+    config = (
+        alg.callbacks(callbacks_class=CustomLoggingCallbacks)
+        .debugging(log_level='ERROR')
+        .environment(env=env_name, clip_actions=env_args.pop('clip_actions', True), **env_args)
+        .env_runners(num_env_runners=4, rollout_fragment_length='auto')
+        .framework(framework=framework_args.pop('framework', 'torch'), **framework_args)
+        .resources(num_gpus=num_gpus)
+        .training(**training_args)
+    )
 
-        # Set algorithm specific defaults
-        if options['algorithm'] == SupportedAlgorithm.PPO:
-            ppo_defaults = {
-                'lambda_': 0.9,
-                'use_gae': True,
-                'clip_param': 0.4,
-                'grad_clip': None,
-                'entropy_coeff': 0.1,
-                'vf_loss_coeff': 0.25,
-                'sgd_minibatch_size': 64,
-                'num_sgd_iter': 10,
-            }
-            training_args = ppo_defaults | training_args
+    # Set run arg defaults
+    run_args = config_dict.get('run_args', {})
+    run_args.setdefault('stop', {'training_iteration': training_iteration})
+    run_args.setdefault('checkpoint_freq', 10)
+    run_args.setdefault('checkpoint_at_end', True)
+    run_args.setdefault('storage_path', Path('./logs').resolve())
+    run_args.setdefault('config', config.to_dict())
 
-        # Setup config with callbacks, debugging, training, etc.
-        alg = PPOConfig() if options['algorithm'] == SupportedAlgorithm.PPO else DQNConfig()
-        config = (
-            alg.callbacks(callbacks_class=CustomLoggingCallbacks)
-            .debugging(log_level='ERROR')
-            .environment(env=env_name, clip_actions=env_args.pop('clip_actions', True), **env_args)
-            .env_runners(num_env_runners=4, rollout_fragment_length='auto')
-            .framework(framework=framework_args.pop('framework', 'torch'), **framework_args)
-            .resources(num_gpus=options['num_gpus'])
-            .training(**training_args)
-        )
+    # Dispatch run
+    result = run(
+        algorithm,
+        name=algorithm,
+        **run_args,
+    )
 
-        # Set run arg defaults
-        run_args = config_dict.get('run_args', {})
-        run_args.setdefault('stop', {'training_iteration': options['training_iteration']})
-        run_args.setdefault('checkpoint_freq', 10)
-        run_args.setdefault('checkpoint_at_end', True)
-        run_args.setdefault('storage_path', Path('./logs').resolve())
-        run_args.setdefault('config', config.to_dict())
+    checkpoint = result.get_last_checkpoint()
+    if checkpoint is None:
+        raise Exception('Last checkpoint not found!')
 
-        # Dispatch run
-        result = run(
-            options['algorithm'].value,
-            name=options['algorithm'].value,
-            **run_args,
-        )
-
-        checkpoint = result.get_last_checkpoint()
-        if checkpoint is None:
-            raise Exception('Last checkpoint not found!')
-
-        # Normalize path to start at the `fastapi` directory
-        checkpoint_path = checkpoint.path.removeprefix('/app/')
-        return checkpoint_path
+    # Normalize path to start at the `fastapi` directory
+    checkpoint_path = checkpoint.path.removeprefix('/app/')
+    return checkpoint_path
