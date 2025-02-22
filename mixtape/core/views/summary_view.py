b3f8502bb458936e67342ef2ebad19eae0240e51
Brianna Major <brianna.major@kitware.com>
2025-02-22 12:24:38 -0500

    Validate view episode_id

--- mixtape/core/views/summary_view.py
+++ mixtape/core/views/summary_view.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from mixtape.core.models.step import Step
 
 
 def simple_view(request, episode_id):
-    steps = Step.objects.filter(episode_id=episode_id)
+    steps = get_list_or_404(
+        Step.objects.filter(episode_id=episode_id).prefetch_related('agent_steps')
+    )
     return render(request, 'simple_view.html', {'steps': steps})
