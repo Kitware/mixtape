a9a6efa3b60f06bf0435c91c4937014508e3a38e
Brianna Major <brianna.major@kitware.com>
2025-02-22 12:29:06 -0500

    Namespace templates

--- mixtape/core/views/summary_view.py
+++ mixtape/core/views/summary_view.py
@@ -7,4 +7,4 @@ def simple_view(request, episode_id):
     steps = get_list_or_404(
         Step.objects.filter(episode_id=episode_id).prefetch_related('agent_steps')
     )
-    return render(request, 'simple_view.html', {'steps': steps})
+    return render(request, 'core/simple_view.html', {'steps': steps})
