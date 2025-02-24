a733f9c6d2e34278f0c4f7cbe7a0e27594afcae8
Brian Helba <brian.helba@kitware.com>
2025-02-24 18:46:03 -0500

    Show the episode ID on the insights page

--- mixtape/core/views.py
+++ mixtape/core/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_object_or_404, render
 
 from mixtape.core.models.episode import Episode
 from mixtape.core.models.step import Step
 
 
 def insights(request: HttpRequest, episode_pk: int) -> HttpResponse:
-    steps = get_list_or_404(Step.objects.prefetch_related('agent_steps'), episode=episode_pk)
-    return render(request, 'core/insights.html', {'steps': steps})
+    episode = get_object_or_404(Episode, pk=episode_pk)
+    steps = Step.objects.prefetch_related('agent_steps').filter(episode=episode_pk)
+    return render(request, 'core/insights.html', {'episode': episode, 'steps': steps})
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
