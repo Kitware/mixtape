5f6fc97c8f8ffb5f75b6d2575e73556b04775383
Brian Helba <brian.helba@kitware.com>
2025-02-24 18:42:21 -0500

    Add type annotations to view functions

--- mixtape/core/views.py
+++ mixtape/core/views.py
@@ -1,14 +1,15 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_list_or_404, render
 
 from mixtape.core.models.episode import Episode
 from mixtape.core.models.step import Step
 
 
-def insights(request, episode_pk):
+def insights(request: HttpRequest, episode_pk: int) -> HttpResponse:
     steps = get_list_or_404(Step.objects.prefetch_related('agent_steps'), episode=episode_pk)
     return render(request, 'core/insights.html', {'steps': steps})
 
 
-def home_page(request):
+def home_page(request: HttpRequest) -> HttpResponse:
     episodes = Episode.objects.all()
     return render(request, 'core/home.html', {'episodes': episodes})
