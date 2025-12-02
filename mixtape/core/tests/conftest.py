2627cdfb62d9adabf7400b1d4c03c6233253a56d
Brian Helba <brian.helba@kitware.com>
2025-12-02 10:54:00 -0500

    Apply upstream changes from Resonant 0.43.0

--- mixtape/core/tests/conftest.py
+++ mixtape/core/tests/conftest.py
@@ -1,22 +1,7 @@
-from django.contrib.auth.models import User
 import pytest
 from rest_framework.test import APIClient
-
-from .factories import UserFactory
 
 
 @pytest.fixture
 def api_client() -> APIClient:
     return APIClient()
-
-
-@pytest.fixture
-def authenticated_api_client(user) -> APIClient:
-    client = APIClient()
-    client.force_authenticate(user=user)
-    return client
-
-
-@pytest.fixture
-def user() -> User:
-    return UserFactory.create()
