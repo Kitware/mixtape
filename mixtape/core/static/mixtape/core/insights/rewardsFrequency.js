05520bc596f9f1b9fe0841b184006c19d0404e91
Brian Helba <brian.helba@kitware.com>
2025-12-05 01:18:42 -0500

    Move the `rewardsFrequency` data to a dedicated file

---
+++ mixtape/core/static/mixtape/core/insights/rewardsFrequency.js
@@ -0,0 +1,57 @@
+document.addEventListener('alpine:init', () => {
+  Alpine.data('rewardsFrequency', () => ({
+    data: [],
+    layout: {
+      title: {text: 'Rewards VS Frequency'},
+      xaxis: {title: {text: 'Reward'}},
+      yaxis: {title: {text: 'Frequency'}},
+      autosize: true,
+      barmode: 'overlay',
+      showlegend: Alpine.store('insights').visOptions.includes('plotLegends'),
+      paper_bgcolor: Alpine.store('theme').paper_bgcolor,
+      plot_bgcolor: Alpine.store('theme').plot_bgcolor,
+      font: Alpine.store('theme').font,
+      xaxis: {
+        gridcolor: Alpine.store('theme').axis.gridcolor,
+        zerolinecolor: Alpine.store('theme').axis.zerolinecolor
+      },
+      yaxis: {
+        gridcolor: Alpine.store('theme').axis.gridcolor,
+        zerolinecolor: Alpine.store('theme').axis.zerolinecolor
+      },
+    },
+    config: {
+      displayModeBar: false,
+      responsive: true
+    },
+    plot: null,
+    init() {
+      this.rewardHistogram.forEach((episode, idx) => {
+        this.data.push({
+          x: episode,
+          type: 'histogram',
+          name: `Episode ${this.episodeIds[idx]}`,
+          marker: {
+            line: {
+              width: 2
+            }
+          }
+        });
+      });
+      this.plot = Plotly.newPlot(this.$refs.rewardsFrequency, this.data, this.layout, this.config);
+      this.$nextTick(() => { Plotly.Plots.resize(this.$refs.rewardsFrequency); });
+      window.addEventListener('resize', () => { Plotly.Plots.resize(this.$refs.rewardsFrequency); });
+      this.$watch('$store.insights.visOptions', () => {
+        this.$nextTick(() => {
+          Plotly.relayout(
+            this.$refs.rewardsFrequency,
+            {
+              autosize: true,
+              showlegend: this.$store.insights.visOptions.includes('plotLegends')
+            })
+        });
+      });
+
+    },
+  }));
+});
