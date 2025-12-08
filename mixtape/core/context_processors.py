d7ada8b5d3233298e5ce2bcbf70dbbdb903a6f71
Brian Helba <brian.helba@kitware.com>
2025-12-08 12:08:19 -0500

    Rename the `MIXTAPE_FEEDBACK_MODAL_ENABLED` setting
    This makes it obvious that it's a MIXTAPE-specific setting.
    
    Also clean up some unnecessary defensive code.

--- mixtape/core/context_processors.py
+++ mixtape/core/context_processors.py
@@ -4,5 +4,5 @@ from django.conf import settings
 from django.http import HttpRequest
 
 
-def demo_feedback_modal(_request: HttpRequest) -> dict[str, bool]:
-    return {'demo_feedback_modal_enabled': getattr(settings, 'DEMO_FEEDBACK_MODAL_ENABLED', False)}
+def mixtape_feedback_modal_enabled(_request: HttpRequest) -> dict[str, bool]:
+    return {'mixtape_feedback_modal_enabled': settings.MIXTAPE_FEEDBACK_MODAL_ENABLED}
